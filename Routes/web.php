afb6ddde524a97ffe957f50d908b0ceac9196635
Michelangelo <sisma99@gmail.com>
2019-06-23 16:46:13 +0200

    menu added

--- Routes/web.php
+++ Routes/web.php
@@ -5,18 +5,21 @@
 | Module Web Routes
 |--------------------------------------------------------------------------
 */
+Route::group(['domain' => config('app.normalurl')], function () {
 
-Route::resource('/library', 'LibraryController');
+    Route::resource('/library', 'LibraryController');
 
-Route::group(['middleware' => 'auth', 'prefix' => 'library/loan'], function (){
+    Route::group(['middleware' => 'auth', 'prefix' => 'library/loan'], function (){
 
-    Route::get('/my', 'LoanController@index')->name('library.loan');
+        Route::get('/my', 'LoanController@index')->name('library.loan');
 
-    Route::group(['middleware' => 'permission:library.loan'], function (){
-        Route::get('/{id}', 'LoanController@loan')->name('library.loan.index');
-        Route::post('/{id}', 'LoanController@store')->name('library.loan.store');
+        Route::group(['middleware' => 'permission:library.loan'], function (){
+            Route::get('/{id}', 'LoanController@loan')->name('library.loan.index');
+            Route::post('/{id}', 'LoanController@store')->name('library.loan.store');
 
-        Route::get('/search/users', 'LoanController@search')->name('library.loan.search');
+            Route::get('/search/users', 'LoanController@search')->name('library.loan.search');
+        });
+
     });
 
 });
